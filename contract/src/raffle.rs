3f1c1bde7525559105ed937a627b663f8b8aef0f
Magamedrasul Ibragimov <curryrasul@gmail.com>
2022-03-14 21:33:34 +0300

    Refactoring

---
+++ contract/src/raffle.rs
@@ -0,0 +1,31 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::Serialize;
+use near_sdk::{AccountId, Balance};
+
+pub type TokenId = String;
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct NFT {
+    pub smart_contract: AccountId,
+    pub id: TokenId,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum Status {
+    Opened,
+    Closed,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Raffle {
+    pub creator: AccountId,
+    pub prize: NFT,
+    pub participants_number: u32,
+    pub participants: Vec<AccountId>,
+    pub ticket_price: Balance,
+    pub status: Status,
+    pub winner: Option<AccountId>,
+}
